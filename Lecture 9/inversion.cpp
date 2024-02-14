df7c73d2a6725483c4fc05b30cc7dce86e80bfd3
Rudra Sharma <rudracollegeclass@gmail.com>
2024-02-14 14:58:47 +0530

    selection sort

---
+++ Lecture 9/inversion.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int getInversions(int arr[], int n)
+{
+    int ans = 0;
+    for(int i = 0;i<n-1;i++){
+        int minInd = i;
+        for(int j = i+1;j<n;j++){
+            if(arr[j]<arr[minInd]){
+                minInd=j;
+            }
+        }
+        int temp = arr[i];
+        arr[i]=arr[minInd];
+        arr[minInd]=temp;
+        ans = ans+(minInd-i);
+    }
+    return ans;
+}
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+	    cin>>arr[i];
+    }
+    int ans = getInversions(arr, n);
+    cout << ans << endl;
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
+
