e826d79b8758a8a6ba5989e2cbc707cdd08aaf95
Rudra Sharma <rudracollegeclass@gmail.com>
2024-04-10 13:36:31 +0530

    linked list

--- Data Structures in C++/LinkedList1/LL.cpp
+++ Data Structures in C++/LinkedList1/LL.cpp
@@ -257,6 +257,79 @@ bool isPalindrome(Node *head) {
 
     return true;
 }
+
+Node *midPoint(Node *head)
+{
+	if(head == NULL){
+		return head;
+	}
+    Node *slow = head;
+	Node * fast = head->next;
+	while(fast!=NULL&&fast->next!=NULL){
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return slow;
+}
+
+
+bool detectCycle(Node *head)
+{
+	Node * slow= head;
+    Node * fast = head->next;
+    while(fast!=NULL   && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(fast == slow){
+            return true;;
+        }
+    }
+
+    return false;
+}
+
+Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2) {
+    if (head1 == NULL) {
+        return head2;
+    }
+    if (head2 == NULL) {
+        return head1;
+    }
+
+    Node *fh = NULL;
+    Node *ft = NULL; 
+
+    if (head1->data < head2->data) {
+        fh = head1;
+        ft = head1;
+        head1 = head1->next;
+    } else {
+        fh = head2;
+        ft = head2;
+        head2 = head2->next;
+    }
+
+    while (head1 != NULL && head2 != NULL) {
+        if (head1->data < head2->data) {
+            ft->next = head1;
+            ft = head1;
+            head1 = head1->next;
+        } else {
+            ft->next = head2;
+            ft = head2;
+            head2 = head2->next;
+        }
+    }
+
+    if (head1 != NULL) {
+        ft->next = head1;
+    }
+    if (head2 != NULL) {
+        ft->next = head2;
+    }
+
+    return fh;
+}
 int main(){
     // static
     // Node n1(10);
