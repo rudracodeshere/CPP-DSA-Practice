c964376e8edc5da590c04edc1ac7932580b2dcde
Rudra Sharma <rudracollegeclass@gmail.com>
2024-04-17 22:14:14 +0530

    stack

--- Data Structures in C++/stacks And Queue/main.cpp
+++ Data Structures in C++/stacks And Queue/main.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
 #include "stackUsingArray.cpp"
+#include<stack>
+bool isBalanced(string expression) {
+  if (expression[0] == ')') {
+    return false;
+  }
+
+  stack<char> s;
+  for (int i = 0; i < expression.size(); i++) {
+    if (expression[i] == '(') {
+      s.push('(');
+    } else if (expression[i] == ')') {
+      if (s.empty() || s.top() != '(') {
+        return false;
+      } else {
+        s.pop();
+      }
+    }
+  }
+
+  return s.empty();
+}
 int main(){
     stackUsingArray s;
     s.push(10);
